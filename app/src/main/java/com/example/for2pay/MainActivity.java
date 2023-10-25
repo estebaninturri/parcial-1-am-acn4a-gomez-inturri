6333d9ddb5b59aa34af1a15146accc9fd2401542
JGomezCannon <jgomez@cannonpuntana.com>
2023-10-25 18:20:12 -0300

    Function, y correccion de vista

--- app/src/main/java/com/example/for2pay/MainActivity.java
+++ app/src/main/java/com/example/for2pay/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.for2pay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//prueba de commit
-
+        //prueba de commit
+    }
+    public void SeleccionarElemento(Button v) {
+        Button myText = new Button(this);
+        myText.setText("Agregar Boton Ejecutado");
     }
 }
