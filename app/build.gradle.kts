5324836c6605fdb17d2a02d68546b1665dde3571
Matías Ezequiel Molina <75143377+JooGo01@users.noreply.github.com>
2023-10-25 18:51:17 -0300

    Function y correcion de vista

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.for2pay"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.for2pay"
         minSdk = 19
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/for2pay/MainActivity.java
+++ app/src/main/java/com/example/for2pay/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.for2pay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //prueba de commit
     }
-    public void SeleccionarElemento(Button v) {
-        Button myText = new Button(this);
-        myText.setText("Agregar Boton Ejecutado");
+    public void agregarElemento(View vista) {
+        LinearLayout contenedor = new LinearLayout(vista.getContext());
+        contenedor.setLayoutParams(new LinearLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        contenedor.setOrientation(LinearLayout.VERTICAL);
+
+        TextView elemento = new TextView(getApplicationContext());
+        elemento.setText("Shell 14/11 $3000");
+        elemento.setBackgroundColor(Color.YELLOW);
+
+        contenedor.addView(elemento);
+
+        FrameLayout.LayoutParams params;
+        params = new FrameLayout.LayoutParams(400, 1500, Gravity.CENTER);
+
+        addContentView(contenedor, params);
+
     }
 }
